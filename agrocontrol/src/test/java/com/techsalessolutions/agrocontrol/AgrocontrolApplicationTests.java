3bf4000b626eba29cd677052ad0a2378842c7ed2
Jose Bermudez <jbpestudio@gmail.com>
2024-02-14 20:07:46 -0500

    se ajustan los archivos

--- agrocontrol/src/test/java/com/techsalessolutions/agrocontrol/AgrocontrolApplicationTests.java
+++ agrocontrol/src/test/java/com/techsalessolutions/agrocontrol/AgrocontrolApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.techsalessolutions.agrocontrol;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AgrocontrolApplicationTests {
 
 	@Test
 	void contextLoads() {
