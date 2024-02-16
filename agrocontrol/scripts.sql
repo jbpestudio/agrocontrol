de207ea1a5fe86c96a0baf2ece71d524aebafe5e
Jose Bermudez <jbpestudio@gmail.com>
2024-02-15 20:13:59 -0500

    Se incluyen nuevas tablas al script de creacion

--- agrocontrol/scripts.sql
+++ agrocontrol/scripts.sql
@@ -1,6 +1,6 @@
-drop table public.farm;
+drop table if exists public.thfarm;
 
-CREATE TABLE public.farm (
+CREATE TABLE public.thfarm (
 	codfarm int4 NOT NULL,
 	namefarm varchar NULL,
 	owner_name varchar NULL,
@@ -11,3 +11,23 @@ CREATE TABLE public.farm (
 	create_up timestamp NULL,
 	CONSTRAINT farm_pkey PRIMARY KEY (codfarm)
 );
+
+drop table if exists public.throl;
+CREATE TABLE public.throl(
+	codrol int4 NOT NULL,
+	namerole varchar null,
+	status int4 DEFAULT 1,
+	create_at timestamp DEFAULT CURRENT_TIMESTAMP,
+	create_up timestamp NULL
+);
+
+drop table if exists public.thlocation;
+CREATE TABLE public.thlocation(
+	codlocation int4 NOT NULL,
+	namelocation varchar null,
+	status int4 DEFAULT 1,
+	create_at timestamp DEFAULT CURRENT_TIMESTAMP,
+	create_up timestamp NULL
+);
+
+
